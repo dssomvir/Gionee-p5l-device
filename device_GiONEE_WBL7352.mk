4387beff25e64bfac591db7ec7dbdc927393d407
dssomvir <dssomvir@gmail.com>
2020-05-17 15:15:37 +0530

    update for wifi fix

--- device_GiONEE_WBL7352.mk
+++ device_GiONEE_WBL7352.mk
@@ -167,7 +167,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Odex
-WITH_DEXPREOPT := true
+WITH_DEXPREOPT := false
 DONT_DEXPREOPT_PREBUILTS := true
 
 # These additionals go to /default.prop
